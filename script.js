e8d3cb0b828dfc7eca1071bf5c1dc670c572bee0
sdlima4025 <sergiolimafg3@gmail.com>
2023-09-20 18:45:16 -0300

    estudando Typescript criado projeto base

---
+++ script.js
@@ -0,0 +1,9 @@
+/**
+ * Contextual Typing em funções anônimas
+ */
+let names = ['Sérgio', 'Pedro', 'Cristina', 'Silvio'];
+
+// função anonima 
+names.forEach(function(nome) {
+  console.log(nome.toUpperCase());  
+} );
