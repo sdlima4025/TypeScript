7d12a4ad51700329a96356c97edc416b0818a803
sdlima4025 <sergiolimafg3@gmail.com>
2023-09-22 09:47:54 -0300

    types primitivos

--- src/script.js
+++ src/script.js
@@ -7,12 +7,6 @@ var botao = document.getElementById("calcular");
 var res = document.getElementById("resultado");
 function calcular(n1, n2) {
     return n1 + n2;
-    // if (typeof n1 == `number` && typeof n2 == `number`) {
-    //   return n1 + n2;
-    // } else {
-    // n1 = parseInt(n1);
-    // n2 = parseInt(n2);
-    // forma abreviada de declarar inteiros
 }
 botao.addEventListener("click", function () {
     res.innerHTML = calcular(+numero1.value, +numero2.value).toString();
--- src/script.ts
+++ src/script.ts
@@ -1,6 +1,6 @@
 /**"Toda vez que houver uma mudança tem que executar o 
  * comando de conversão"comando para converter 
- * o js em TS === tsc src/script.ts --outDir Public */
+ * o js em TS === tsc src/script.ts --outDir Public 
  
 let numero1 = document.getElementById("numero1") as HTMLInputElement;
 let numero2 = document.getElementById("numero2") as HTMLInputElement;
@@ -9,15 +9,18 @@ let res = document.getElementById("resultado") as HTMLDivElement;
 
 function calcular(n1:number, n2:number) {
   return n1 + n2;
-  // if (typeof n1 == `number` && typeof n2 == `number`) {
-  //   return n1 + n2;
-  // } else {
-    // n1 = parseInt(n1);
-    // n2 = parseInt(n2);
-    // forma abreviada de declarar inteiros
+ 
   }
-
 
 botao.addEventListener("click", function () {
   res.innerHTML = calcular(+numero1.value, +numero2.value).toString();
-});
+}); */
+
+// Os types primitivos | use as tipagem sempre em minusculos
+// let nome: string = 'Sérgio';
+
+// let idade: number = 53;
+
+// let programador: boolean = true;
+
+
